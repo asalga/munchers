a7a1bcd6c217d78792cf107c610cd23e9d8ae23c
Andor Saga <andor.saga@gmail.com>
2018-01-16 17:09:25 -0500

    .

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+const TileWidth = 28;
+const TileHeight = 24;
+
 let cvs = document.getElementById('screen');
 let ctx = cvs.getContext('2d');
 
@@ -5,7 +8,7 @@ function loadImage(url) {
     return new Promise(function(resolve) {
         let img = new Image;
         img.onload = function() {
-        	resolve(img);
+            resolve(img);
         };
         img.src = url;
     });
@@ -14,7 +17,9 @@ function loadImage(url) {
 ctx.fillStyle = 'rgba(0,0,0,255)';
 ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-loadImage('images/muncher.png')
-    .then(function(img) {
-        ctx.drawImage(img, 0, 0);
-    })
+loadImage('/images/muncher.png')
+    .then(img => ctx.drawImage(img,
+        0, 0,
+        TileWidth, TileHeight,
+        0, 0,
+        TileWidth, TileHeight))
